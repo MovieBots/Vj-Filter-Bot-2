9f39b2ec555806ddc9e0759d17d9557fb7a4af34
BotMaster135 <amirbhaiyt1@gmail.com>
2024-08-28 13:24:06 +0530

    Update info.py

--- info.py
+++ info.py
@@ -71,10 +71,10 @@ CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "") # Necessary If clone 
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+pylTdXC-Tf4zNDc1')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+pylTdXC-Tf4zNDc1')
-TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_To_Open_Linkl')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/+pylTdXC-Tf4zNDc1') # Support Chat Link Without https:// or @
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/SimpleNew4')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/TG_viewsboster_service')
+TUTORIAL = environ.get('TUTORIAL', 'https://t.me/TG_viewsboster_service')
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/MovieSearch208') # Support Chat Link Without https:// or @
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
