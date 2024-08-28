98d2ff73947b539f1a1f37d287b6271804af0237
BotMaster135 <amirbhaiyt1@gmail.com>
2024-08-28 14:29:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -56,7 +56,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "techvjautobot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'techvjcollection')
 
 # Premium And Referal Settings
-PREMIUM_AND_REFERAL_MODE = bool(environ.get('PREMIUM_AND_REFERAL_MODE', True)) # Set Ture Or False
+PREMIUM_AND_REFERAL_MODE = bool(environ.get('PREMIUM_AND_REFERAL_MODE', False)) # Set Ture Or False
 
 # If PREMIUM_AND_REFERAL_MODE is True Then Fill Below Variable, If Flase Then No Need To Fill.
 REFERAL_COUNT = int(environ.get('REFERAL_COUNT', '20')) # number of referal count
@@ -71,10 +71,10 @@ CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "") # Necessary If clone 
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+pylTdXC-Tf4zNDc1')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+pylTdXC-Tf4zNDc1')
-TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_To_Open_Linkl')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/+pylTdXC-Tf4zNDc1') # Support Chat Link Without https:// or @
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+DNAqhKkpTf0zYTI1')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+eEMdl7-M_5RmMWNl')
+TUTORIAL = environ.get('TUTORIAL', 'https://t.me/TG_viewsboster_service')
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/+DNAqhKkpTf0zYTI1') # Support Chat Link Without https:// or @
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
